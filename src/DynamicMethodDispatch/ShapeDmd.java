2b8250b6f6834c1d0a74eb3a9b116071c48b68ce
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2022-06-14 16:45:21 +0000

    Experimenting with Dynamic Method Dispatch

---
+++ src/DynamicMethodDispatch/ShapeDmd.java
@@ -0,0 +1,74 @@
+package DynamicMethodDispatch;
+
+class Shape {
+    double a;
+    double b;
+
+    Shape(double i, double j) {
+        a = i;
+        b = j;
+    }
+
+    Shape(double i) {
+        a = b = i;
+    }
+
+    Shape() {
+        a = b = -1;
+    }
+
+    double showCoord() {
+        System.out.println(a + " " + b);
+        return 0;
+    }
+
+    void getCoord(double i, double j) {
+        a = i;
+        b = j;
+    }
+
+    void getCoord(double i) {
+        a = b = i;
+    }
+}
+
+class Rect extends Shape {
+    Rect(double i, double j) {
+        super(i, j);
+    }
+
+    Rect(double i) {
+        super(i);
+    }
+
+    Rect() {
+        super();
+    }
+
+    void getCoord(double i, double j) {
+        super.getCoord(i, j);
+    }
+
+    void getCoord(double i) {
+        super.getCoord(i);
+    }
+
+    double showCoord() {
+        System.out.println("Length: " + a + " Breadth: " + b);
+        return 0;
+    }
+}
+
+public class ShapeDmd {
+    public static void main(String[] args) {
+        Rect myRect = new Rect(10, 20);
+        Rect myRect1 = new Rect(20);
+        Rect r;
+
+        myRect.getCoord(5, 10);
+        myRect1.getCoord(10);
+
+        r = myRect.showCoord(); // Doesn't work
+
+    }
+}
